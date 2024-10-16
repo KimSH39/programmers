09f8b66d7bbcb9dc3d3e4d263420ac3f83377667
Sohyeon Kim <sooooooohyeon5@naver.com>
2024-10-16 14:42:36 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 14.14 ms, Memory: 77.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.java
@@ -0,0 +1,21 @@
+class Solution {
+    public long solution(int a, int b) {
+        long answer = 0;
+        
+        if (a > b) {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        
+        for (int i = a; i < b + 1; i++) {
+            answer += i;
+        }
+        
+        if (a == b) {
+            return a;
+        }
+        
+        return answer;
+    }
+}
